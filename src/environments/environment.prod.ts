ef14647f491ed91822716651a25315e129ce57d8
reynave <felix.renaldi@gmail.com>
2021-10-08 13:12:17 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   title : "KITARO21",
   subtitle : "ORG",
-  admin : " https://admin2.kitaro21.org/admin.api/",
+  admin : "https://admin2.kitaro21.org/admin.api/",
   api : "https://member.kitaro21.org/user.api/",
   coffeetalk : "https://member.coffeetalk.co.id/",  
 };
